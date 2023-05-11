55fe6ec63df57d9222d813170bdb320d07dbad56
HavaAlex <99481724+HavaAlex@users.noreply.github.com>
2023-05-11 19:59:30 +0200

    Add files via upload

---
+++ formazas/formazas/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿
+namespace formazas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(229, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mit:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Font méret";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 19);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(49, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Piros";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(14, 190);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(93, 100);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Szín";
+            
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(6, 64);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(47, 17);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "Zöld";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(6, 41);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(45, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Kék";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox6);
+            this.groupBox2.Controls.Add(this.checkBox4);
+            this.groupBox2.Controls.Add(this.checkBox5);
+            this.groupBox2.Location = new System.Drawing.Point(113, 190);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(75, 100);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Stílus";
+            
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(0, 64);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(67, 17);
+            this.checkBox6.TabIndex = 2;
+            this.checkBox6.Text = "Félkövér";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.CheckedChanged += new System.EventHandler(this.checkBox6_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(0, 19);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(45, 17);
+            this.checkBox4.TabIndex = 0;
+            this.checkBox4.Text = "Dőlt";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(0, 41);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(70, 17);
+            this.checkBox5.TabIndex = 1;
+            this.checkBox5.Text = "Aláhúzott";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButton1);
+            this.groupBox3.Controls.Add(this.radioButton2);
+            this.groupBox3.Controls.Add(this.radioButton3);
+            this.groupBox3.Location = new System.Drawing.Point(194, 190);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(121, 100);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Font";
+            
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(5, 17);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(45, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Arial";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(5, 40);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(115, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Times New Roman";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(5, 63);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(81, 17);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Comic Sans";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Szöveg",
+            "Háttér"});
+            this.comboBox1.Location = new System.Drawing.Point(74, 77);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(84, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(74, 102);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(84, 20);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.textBox1);
+            this.groupBox4.Controls.Add(this.button1);
+            this.groupBox4.Location = new System.Drawing.Point(321, 190);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(200, 100);
+            this.groupBox4.TabIndex = 9;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Új szöveg";
+            
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(188, 20);
+            this.textBox1.TabIndex = 9;
+            
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Átír";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Gyönyörű felirat";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ formazas/formazas/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace formazas
+{
+
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedItem = "Szöveg";
+            numericUpDown1.Value = 10;
+            label1.BackColor = Color.FromArgb(255, 255, 255);
+            label1.Size = new Size(100,50);
+            radioButton1.Checked = true;
+            label1.Text = "Próba";
+            radioButton2.Font = new Font("Times New Roman", radioButton2.Font.Size);
+            radioButton3.Font = new Font("Comic Sans MS", radioButton3.Font.Size);
+            checkBox4.Font = new Font(checkBox4.Font, checkBox4.Font.Style | FontStyle.Bold);
+            checkBox5.Font = new Font(checkBox5.Font, checkBox5.Font.Style | FontStyle.Underline);
+            checkBox6.Font = new Font(checkBox6.Font, checkBox6.Font.Style | FontStyle.Italic);
+            
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Színezés 
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            /*
+            if (label1.BackColor == Color.FromArgb(255, 255, 255))
+            {
+                label1.BackColor = Color.FromArgb(0, 0, 0);
+            }
+            */
+            if (checkBox1.Checked == true)
+            {
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    label1.ForeColor = Color.FromArgb(255, label1.ForeColor.G, label1.ForeColor.B);
+                }
+                else if (comboBox1.SelectedIndex == 1)
+                {
+                    label1.BackColor = Color.FromArgb(255, label1.BackColor.G, label1.BackColor.B);
+                }
+            }
+            else
+            {
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    label1.ForeColor = Color.FromArgb(0, label1.ForeColor.G, label1.ForeColor.B);
+                }
+                else if (comboBox1.SelectedIndex == 1)
+                {
+                    label1.BackColor = Color.FromArgb(0, label1.BackColor.G, label1.BackColor.B);
+                }
+            }
+
+            
+                
+        }
+
+        
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+            
+            if (checkBox2.Checked == true)
+            {
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    label1.ForeColor = Color.FromArgb(label1.ForeColor.R, label1.ForeColor.G, 255);
+                }
+                else if (comboBox1.SelectedIndex == 1)
+                {
+                    label1.BackColor = Color.FromArgb(label1.BackColor.R, label1.BackColor.G, 255);
+                }
+            }
+            else
+            {
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    label1.ForeColor = Color.FromArgb(label1.ForeColor.R, label1.ForeColor.G, 0);
+                }
+                else if (comboBox1.SelectedIndex == 1)
+                {
+                    label1.BackColor = Color.FromArgb(label1.BackColor.R, label1.BackColor.G, 0);
+                }
+            }
+        }
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            if (checkBox3.Checked == true)
+            {
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    label1.ForeColor = Color.FromArgb(label1.ForeColor.R, 255, label1.ForeColor.B);
+                }
+                else if (comboBox1.SelectedIndex == 1)
+                {
+                    label1.BackColor = Color.FromArgb(label1.BackColor.R, 255, label1.BackColor.B);
+                }
+            }
+            else
+            {
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    label1.ForeColor = Color.FromArgb(label1.ForeColor.R, 0, label1.ForeColor.B);
+                }
+                else if (comboBox1.SelectedIndex == 1)
+                {
+                    label1.BackColor = Color.FromArgb(label1.BackColor.R, 0, label1.BackColor.B);
+                }
+            }
+        }
+        // Dőlt, aláhúzott, félkövér
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox4.Checked == true)
+            {
+                label1.Font = new Font(label1.Font, label1.Font.Style | FontStyle.Italic);
+            }
+            else
+            {
+                label1.Font = new Font(label1.Font, label1.Font.Style & ~FontStyle.Italic);
+            }
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox5.Checked == true)
+            {
+                label1.Font = new Font(label1.Font, label1.Font.Style | FontStyle.Underline);
+            }
+            else
+            {
+                label1.Font = new Font(label1.Font, label1.Font.Style & ~FontStyle.Underline);
+            }
+        }
+
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox6.Checked == true)
+            {
+                label1.Font = new Font(label1.Font, label1.Font.Style | FontStyle.Bold);
+            }
+            else
+            {
+                label1.Font = new Font(label1.Font, label1.Font.Style & ~FontStyle.Bold);
+            }
+            
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 1)
+            {
+                groupBox1.Enabled = true;
+                groupBox2.Enabled = false;
+                groupBox3.Enabled = false;
+                numericUpDown1.Enabled = true;
+
+                if (checkBox1.Checked == true)
+                {
+                    checkBox1.Checked = false;
+                }
+                if (checkBox2.Checked == true)
+                {
+                    checkBox2.Checked = false;
+                }
+                if (checkBox3.Checked == true)
+                {
+                    checkBox3.Checked = false;
+                }
+
+                if (checkBox1.Checked == false)
+                {
+                    checkBox1.Checked = true;
+                }
+                if (checkBox2.Checked == false)
+                {
+                    checkBox2.Checked = true;
+                }
+                if (checkBox3.Checked == false)
+                {
+                    checkBox3.Checked = true;
+                }
+
+            }
+            else if (comboBox1.SelectedIndex == 0)
+            {
+                groupBox1.Enabled = true;
+                groupBox2.Enabled = true;
+                groupBox3.Enabled = true; 
+                numericUpDown1.Enabled = true;
+
+               
+
+            }
+        }
+        //Mérettekerés
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            
+            if(comboBox1.SelectedIndex == 1)
+            {
+                label1.Size = new Size(Convert.ToInt32(numericUpDown1.Value) * 5, Convert.ToInt32(numericUpDown1.Value) * 2);
+                numericUpDown1.Maximum = 56;
+                numericUpDown1.Minimum = 10;
+            }
+            if (comboBox1.SelectedIndex == 0)
+            {
+                numericUpDown1.Maximum = 80;
+                numericUpDown1.Minimum = 10;
+                label1.Font = ChangeFontSize(label1.Font, Convert.ToInt32(numericUpDown1.Value));
+            }
+        }
+
+        //netről lopva
+        static public Font ChangeFontSize(Font font, float fontSize)
+        {
+            if (font != null)
+            {
+                float currentSize = font.Size;
+                if (currentSize != fontSize)
+                {
+                    font = new Font(font.Name, fontSize,
+                        font.Style, font.Unit,
+                        font.GdiCharSet, font.GdiVerticalFont);
+                }
+            }
+            return font;
+        }
+
+        //Átírás
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text;
+
+            csere(this);
+        }
+        public void csere(Form ez)
+        {
+            if (textBox1.Text == "taiwan")
+            {
+                ez.BackgroundImage = Properties.Resources.kina;
+            }
+        }
+        
+
+        //Betűtípus
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Font = new Font("Arial", label1.Font.Size);
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Font = new Font("Times New Roman", label1.Font.Size);
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Font = new Font("Comic Sans MS", label1.Font.Size);
+        }
+    }
+}
